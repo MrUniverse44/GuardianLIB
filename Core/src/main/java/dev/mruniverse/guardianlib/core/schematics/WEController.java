c5829836db4190951ef95ca4288cf709246907f4
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-04-03 10:31:40 -0500

    Removed Again WorldEdit-Without-FastAsyncWorldEdit Schematic Support, if you want use schematic system, you need install FastAsyncWorldEdit

--- Core/src/main/java/dev/mruniverse/guardianlib/core/schematics/WEController.java
+++ Core/src/main/java/dev/mruniverse/guardianlib/core/schematics/WEController.java
@@ -1,9 +1,5 @@
 package dev.mruniverse.guardianlib.core.schematics;
 
-import com.sk89q.worldedit.Vector;
-import com.sk89q.worldedit.bukkit.BukkitWorld;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
-import com.sk89q.worldedit.world.World;
 import dev.mruniverse.guardianlib.core.GuardianLIB;
 import dev.mruniverse.guardianlib.core.utils.Logger;
 import org.bukkit.Location;
@@ -13,12 +9,17 @@ import java.io.File;
 public class WEController implements WorldEditController {
     private final Logger logs = GuardianLIB.getControl().getLogs();
     public void pasteSchematic(File schematic, Location location) {
-        World world = new BukkitWorld(location.getWorld());
-        try {
-            Vector vector = new Vector(location.getX(),location.getY(),location.getZ());
-            ClipboardFormat.SCHEMATIC.load(schematic).paste(world, vector, false, true, null);
-        } catch (Throwable throwable) {
-            logs.error("Can't paste schematic :(");
-        }
+        logs.info("Schematic system is disabled for now, please install FastAsyncWorldEdit to use the another schematic system of the plugin, to fix temporal error");
+        logs.info("This is not your error, this version of the plugin don't have WorldEdit-Without-FastAsyncWorldEdit Support");
+        logs.info("Reason: Maven Issues, Class Issue: 'com.sk89q.worldedit.world.registry.WorldData' not found");
+        logs.info("This issue will be fixed in the future.");
+        //World world = new BukkitWorld(location.getWorld());
+        //try {
+            //Vector vector = new Vector(location.getX(),location.getY(),location.getZ());
+            //ClipboardFormat.SCHEMATIC.load(schematic).paste(world, vector, false, true, null);
+
+        //} catch (Throwable throwable) {
+        //    logs.error("Can't paste schematic :(");
+        //}
     }
 }
