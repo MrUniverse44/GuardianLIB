4ad7d9c36109c3fc00622528a2c8e37c7d805b51
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-06-19 16:51:31 -0500

    ActionBar Fix from 1_9_R2 to 1_11_R1

--- v1_11_R1/src/main/java/dev/mruniverse/guardianlib/nms/v1_11_R1/NMSHandler.java
+++ v1_11_R1/src/main/java/dev/mruniverse/guardianlib/nms/v1_11_R1/NMSHandler.java
@@ -11,6 +11,8 @@ import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.minecraft.server.v1_11_R1.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -324,9 +326,7 @@ public final class NMSHandler implements NMS {
 
     public void sendActionBar(Player player, String msg) {
         String toBC = ChatColor.translateAlternateColorCodes('&', msg);
-        IChatBaseComponent icbc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + toBC + "\"}");
-        PacketPlayOutChat bar = new PacketPlayOutChat(icbc, (byte)2);
-        ((CraftPlayer)player).getHandle().playerConnection.sendPacket(bar);
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(toBC));
     }
     public void sendBossBar(Player player, String message) {
         if(!BossHasPlayer(player)) {
