1ee3e4b1b9c0ced7d8a40c4fef5bb2e8d1cd738a
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-10-14 15:57:09 -0500

    Improvement

---
+++ v1_17_R1/src/main/java/dev/mruniverse/guardianlib/nms/v1_17R1/ChunkListener.java
@@ -0,0 +1,31 @@
+package dev.mruniverse.guardianlib.nms.v1_17R1;
+
+import dev.mruniverse.guardianlib.core.GuardianLIB;
+import dev.mruniverse.guardianlib.core.events.GuardianChunkUnloadEvent;
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkUnloadEvent;
+
+public class ChunkListener implements Listener {
+    private final GuardianLIB guardianLIB;
+
+    public ChunkListener(GuardianLIB guardianLIB) {
+        this.guardianLIB = guardianLIB;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST,ignoreCancelled = true)
+    public void onChunkUnload(ChunkUnloadEvent event) {
+
+        GuardianChunkUnloadEvent unloadEvent = new GuardianChunkUnloadEvent(event.getChunk(),event.getWorld());
+        guardianLIB.getServer().getPluginManager().callEvent(unloadEvent);
+        if(unloadEvent.isCancelled()) {
+            World world = event.getWorld();
+            Chunk chunk = event.getChunk();
+
+            world.setChunkForceLoaded(chunk.getX(), chunk.getZ(), true);
+        }
+    }
+}
